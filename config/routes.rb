f515efbb2d835312ee3324678aff664e67f1b900
Joshua Daniel <jrdaniel@ncsu.edu>
2016-07-08 12:44:17 -0400

    Adding a show page for a specific list

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "lists#index"
-end
+  get "specific_list", to: "lists#show" #to: is a symbol
+end 
